bf17718d3f8d88244f63d4af23b9f2cc55dd8b4d
keycoin <keycoin>
2014-07-16 20:14:42 +0000

    Update DNSseed

--- src/net.cpp
+++ src/net.cpp
@@ -1139,7 +1139,7 @@ void MapPort()
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
-    {"node.KeyCoin.net", "node.KeyCoin.net"},
+    {"seed.keycoincrypto.com", "seed.keycoincrypto.com"},
 };
 
 void ThreadDNSAddressSeed(void* parg)
